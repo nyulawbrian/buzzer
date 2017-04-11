d4de171fa8e5ea398d8ff9b1203d61adf3aa7cf7
brian <brian.yulke@nyu.edu>
2017-04-10 23:15:42 -0400

    further Flask.  renamed buzzer to buzzerctl to avoid confusion

---
+++ buzzerweb.py
@@ -0,0 +1,30 @@
+from flask import Flask, request, session, g, redirect, url_for, abort, \
+     render_template, flash
+
+import rpyc
+import automationhat
+import time
+
+# Create application instance
+app = Flask(__name__)
+
+# Load config from this file
+app.config.from_object(__name__)
+
+# Load config
+app.config.update(dict(
+    host='0.0.0.0'
+))
+
+# Set (export) env var to point to a config file
+# app.config.from_envvar('SOMEENVVAR', silent=True)
+
+
+@app.route('/')
+def hello_world():
+    automationhat.light.power.toggle()
+    time.sleep(5)
+    return 'Power light ={0}'.format(automationhat.light.power.read())
+
+
+#EOF
